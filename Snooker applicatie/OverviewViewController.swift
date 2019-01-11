78d6c8854ea20d05d3c323ccc69c8705ebd48aca
Dennis Deroose <dennisderoose@MacBook-Pro-van-Dennis.local>
2019-01-11 18:09:44 +0100

    changed api call to login and when the s

---
+++ Snooker applicatie/OverviewViewController.swift
@@ -0,0 +1,14 @@
+//
+//  OverviewViewController.swift
+//  Snooker applicatie
+//
+//  Created by Dennis Deroose on 11/01/2019.
+//  Copyright © 2019 Dennis Deroose. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class OverviewViewController: UIViewController {
+
+}
--- Snooker applicatie/ViewController.swift
+++ Snooker applicatie/ViewController.swift
@@ -23,21 +23,6 @@ class ViewController: UIViewController {
         btnRegister.layer.cornerRadius = 5
         btnRegister.layer.borderWidth = 1
         
-        let headers = [
-            "Accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded"
-        ]
-        let parameters = [
-            "grant_type": "password",
-            "username": "Dennis",
-            "password": "Test1;"
-        ]
-        Alamofire.request("http://backendapplications.azurewebsites.net/token", method: .post,parameters: parameters, headers: headers).responseJSON { (response) -> Void in
-            
-            if let JSON = response.result.value{
-                print(JSON)
-            }
-        }
         //api call
         /*Alamofire.request("http://backendapplications.azurewebsites.net/api/Breaks")
             .responseJSON{ (response) -> Void in
@@ -49,7 +34,48 @@ class ViewController: UIViewController {
         
         
     }
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
     @IBOutlet weak var btnInlog: UIButton!
     @IBOutlet weak var btnRegister: UIButton!
+    @IBAction func btnInlogPressed(_ sender: UIButton) {
+        let headers = [
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded"
+        ]
+        let parameters = [
+            "grant_type": "password",
+            "username": txtUsername?.text ?? "",
+            "password": txtPassword?.text ?? ""
+            ] as [String : Any]
+        Alamofire.request("http://backendapplications.azurewebsites.net/token", method: .post,parameters: parameters, headers: headers).responseJSON { (response) -> Void in
+            
+            if let statuscode = response.response?.statusCode{
+                if(statuscode == 400){
+                    let toastLabel = UILabel(frame: CGRect(x: self.view.frame.size.width/2 - 75, y: self.view.frame.size.height-100, width: 150, height: 35))
+                    toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+                    toastLabel.textColor = UIColor.white
+                    toastLabel.textAlignment = .center;
+                    toastLabel.font = UIFont(name: "Montserrat-Light", size: 12.0)
+                    toastLabel.text = "Er is een foute username of paswoord ingevuld"
+                    toastLabel.alpha = 1.0
+                    toastLabel.layer.cornerRadius = 10;
+                    toastLabel.clipsToBounds  =  true
+                    self.view.addSubview(toastLabel)
+                    UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                        toastLabel.alpha = 0.0
+                    }, completion: {(isCompleted) in
+                        toastLabel.removeFromSuperview()
+                    })
+                } else if(statuscode == 200){
+                    let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+                    let newViewController = storyBoard.instantiateViewController(withIdentifier: "overviewView")
+                    self.present(newViewController, animated: true, completion: nil)
+                } else {
+                    
+                }
+            }
+        }
+    }
 }
 
