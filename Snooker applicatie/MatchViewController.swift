9b6425c5465261153ae56ceba999d177450344ba
Dennis Deroose <dennisderoose@MacBook-Pro-van-Dennis.local>
2019-01-16 21:57:56 +0100

    Added that you can play multiple frames in a match and if the match is done that the match is updated

--- Snooker applicatie/MatchViewController.swift
+++ Snooker applicatie/MatchViewController.swift
@@ -21,6 +21,8 @@ class MatchViewController: UIViewController{
     var dateOfMatch: String!
     
     var frameId: Int!
+    var framesWonPlayer1: Int! = 0
+    var framesWonPlayer2: Int! = 0
 
     @IBOutlet weak var btnAddScorePlayer1: UIButton!
     @IBOutlet weak var lblPlayer1: UILabel!
@@ -34,7 +36,144 @@ class MatchViewController: UIViewController{
     @IBOutlet weak var btnEndFrame: UIButton!
     @IBOutlet weak var btnEndMatch: UIButton!
     @IBAction func btnEndMatchTapped(_ sender: Any) {
-        self.performSegue(withIdentifier: "endMatchSegue", sender: self)
+        if(Int(lblScorePlayer1.text!)! >= Int(lblScorePlayer2.text!)!){
+            framesWonPlayer1 = framesWonPlayer1+1
+            let parameters = [
+                "FrameId": frameId,
+                "DurationFrame": 0.0,
+                "PointsWinner": Int(lblScorePlayer1.text!)!,
+                "PointsOpponent": Int(lblScorePlayer2.text!)!,
+                "Winner": player!,
+                "MatchId": matchId
+                ] as [String : Any]
+            Alamofire.request("http://backendapplications.azurewebsites.net/api/Frames/\(frameId!)", method: .put,parameters: parameters).responseJSON { (response) -> Void in
+                if let statuscode = response.response?.statusCode{
+                    if(statuscode == 204){
+                        
+                    } else{
+                        let errorss = response.description
+                        let errorsplit = errorss.components(separatedBy: "\"")
+                        var errorMerge = ""
+                        for i in stride(from: 9, to: errorsplit.count, by: 4) {
+                            errorMerge = errorMerge + errorsplit[i]
+                        }
+                        
+                        
+                        let toastLabel = UILabel(frame: CGRect(x: 5, y: self.view.frame.size.height-100, width: (self.view.frame.width - 10), height: 70))
+                        toastLabel.numberOfLines = 2
+                        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+                        toastLabel.textColor = UIColor.white
+                        toastLabel.textAlignment = .center;
+                        toastLabel.font = UIFont(name: "Montserrat-Light", size: 10.0)
+                        toastLabel.text = errorMerge
+                        toastLabel.alpha = 1.0
+                        toastLabel.layer.cornerRadius = 10;
+                        toastLabel.clipsToBounds  =  true
+                        self.view.addSubview(toastLabel)
+                        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                            toastLabel.alpha = 0.0
+                        }, completion: {(isCompleted) in
+                            toastLabel.removeFromSuperview()
+                        })
+                    }
+                }
+            }
+            
+        } else {
+            framesWonPlayer2 = framesWonPlayer2+1
+            let parameters = [
+                "FrameId": frameId,
+                "DurationFrame": 0.0,
+                "PointsWinner": Int(lblScorePlayer1.text!)!,
+                "PointsOpponent": Int(lblScorePlayer2.text!)!,
+                "Winner": opponent!,
+                "MatchId": matchId
+                ] as [String : Any]
+            Alamofire.request("http://backendapplications.azurewebsites.net/api/Frames/\(frameId!)", method: .put,parameters: parameters).responseJSON { (response) -> Void in
+                if let statuscode = response.response?.statusCode{
+                    if(statuscode == 204){
+                        
+                    } else{
+                        let errorss = response.description
+                        let errorsplit = errorss.components(separatedBy: "\"")
+                        var errorMerge = ""
+                        for i in stride(from: 9, to: errorsplit.count, by: 4) {
+                            errorMerge = errorMerge + errorsplit[i]
+                        }
+                        
+                        
+                        let toastLabel = UILabel(frame: CGRect(x: 5, y: self.view.frame.size.height-100, width: (self.view.frame.width - 10), height: 70))
+                        toastLabel.numberOfLines = 2
+                        toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+                        toastLabel.textColor = UIColor.white
+                        toastLabel.textAlignment = .center;
+                        toastLabel.font = UIFont(name: "Montserrat-Light", size: 10.0)
+                        toastLabel.text = errorMerge
+                        toastLabel.alpha = 1.0
+                        toastLabel.layer.cornerRadius = 10;
+                        toastLabel.clipsToBounds  =  true
+                        self.view.addSubview(toastLabel)
+                        UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                            toastLabel.alpha = 0.0
+                        }, completion: {(isCompleted) in
+                            toastLabel.removeFromSuperview()
+                        })
+                    }
+                }
+            }
+        }
+        let jwt = try! decode(jwt: Constants.token)
+        let name = jwt.claim(name: "http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier")
+        var id = ""
+        if let idUser = name.string {
+            id = idUser
+        }
+        print(dateOfMatch!)
+        let parameters = [
+            "MatchId": matchId,
+            "PotSucces": 0.0,
+            "Player": player!,
+            "Opponent": opponent!,
+            "SafetySucces": 0.0,
+            "DurationMatch": "2018-12-28T14:00:00",
+            "DateOfMatch": dateOfMatch!,
+            "NumberMatchesWonPlayer1": framesWonPlayer1,
+            "NumberMatchesWonPlayer2": framesWonPlayer2,
+            "OpslaanBreak": saveBreak,
+            "UserId": id
+            ] as [String : Any]
+        Alamofire.request("http://backendapplications.azurewebsites.net/api/Matches/\(matchId!)", method: .put,parameters: parameters).responseJSON { (response) -> Void in
+            if let statuscode = response.response?.statusCode{
+                if(statuscode == 204){
+                    self.performSegue(withIdentifier: "endMatchSegue", sender: self)
+                } else{
+                    let errorss = response.description
+                    let errorsplit = errorss.components(separatedBy: "\"")
+                    var errorMerge = ""
+                    for i in stride(from: 9, to: errorsplit.count, by: 4) {
+                        errorMerge = errorMerge + errorsplit[i]
+                    }
+                    
+                    
+                    let toastLabel = UILabel(frame: CGRect(x: 5, y: self.view.frame.size.height-100, width: (self.view.frame.width - 10), height: 70))
+                    toastLabel.numberOfLines = 2
+                    toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+                    toastLabel.textColor = UIColor.white
+                    toastLabel.textAlignment = .center;
+                    toastLabel.font = UIFont(name: "Montserrat-Light", size: 10.0)
+                    toastLabel.text = errorMerge
+                    toastLabel.alpha = 1.0
+                    toastLabel.layer.cornerRadius = 10;
+                    toastLabel.clipsToBounds  =  true
+                    self.view.addSubview(toastLabel)
+                    UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                        toastLabel.alpha = 0.0
+                    }, completion: {(isCompleted) in
+                        toastLabel.removeFromSuperview()
+                    })
+                }
+            }
+        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -259,6 +398,7 @@ class MatchViewController: UIViewController{
             btnEndFrame.isHidden = true
         }
         if(Int(lblScorePlayer1.text!)! >= Int(lblScorePlayer2.text!)!){
+            framesWonPlayer1 = framesWonPlayer1+1
             let parameters = [
                 "FrameId": frameId,
                 "DurationFrame": 0.0,
@@ -301,6 +441,7 @@ class MatchViewController: UIViewController{
             }
             
         } else {
+            framesWonPlayer2 = framesWonPlayer2+1
             let parameters = [
                 "FrameId": frameId,
                 "DurationFrame": 0.0,
@@ -340,6 +481,50 @@ class MatchViewController: UIViewController{
                         })
                     }
                 }
+            }
+        }
+        let parameters = [
+            "FrameId": 80,
+            "DurationFrame": 0.0,
+            "PointsWinner": 0,
+            "PointsOpponent": 0,
+            "Winner": "",
+            "MatchId": matchId
+            ] as [String : Any]
+        Alamofire.request("http://backendapplications.azurewebsites.net/api/Frames", method: .post,parameters: parameters).responseJSON { (response) -> Void in
+            if let statuscode = response.response?.statusCode{
+                if(statuscode == 201){
+                    let convertedString = String(describing: response.result.value!)
+                    let separatedSpace = convertedString.components(separatedBy: " ")
+                    let separatedPointKomma = separatedSpace[18].components(separatedBy: ";")
+                    self.frameId = Int(separatedPointKomma[0])!
+                } else{
+                    let errorss = response.description
+                    let errorsplit = errorss.components(separatedBy: "\"")
+                    var errorMerge = ""
+                    for i in stride(from: 9, to: errorsplit.count, by: 4) {
+                        errorMerge = errorMerge + errorsplit[i]
+                    }
+                    
+                    
+                    let toastLabel = UILabel(frame: CGRect(x: 5, y: self.view.frame.size.height-100, width: (self.view.frame.width - 10), height: 70))
+                    toastLabel.numberOfLines = 2
+                    toastLabel.backgroundColor = UIColor.black.withAlphaComponent(0.6)
+                    toastLabel.textColor = UIColor.white
+                    toastLabel.textAlignment = .center;
+                    toastLabel.font = UIFont(name: "Montserrat-Light", size: 10.0)
+                    toastLabel.text = errorMerge
+                    toastLabel.alpha = 1.0
+                    toastLabel.layer.cornerRadius = 10;
+                    toastLabel.clipsToBounds  =  true
+                    self.view.addSubview(toastLabel)
+                    UIView.animate(withDuration: 4.0, delay: 0.1, options: .curveEaseOut, animations: {
+                        toastLabel.alpha = 0.0
+                    }, completion: {(isCompleted) in
+                        toastLabel.removeFromSuperview()
+                    })
+                }
+                
             }
         }
         playedFrames = playedFrames+1
